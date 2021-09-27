10bfe0ec615e6dc885427db87bbc65647c0b4f9c
NickerSK <nickersk@gmail.com>
2021-09-27 10:29:31 +0200

    pythonmodule -> py3

--- servicemp3/pythonmodule.cpp
+++ servicemp3/pythonmodule.cpp
@@ -1,13 +1,25 @@
 #include "servicemp3.h"
 
 
-static PyMethodDef servicemp3Methods[] =
+static PyMethodDef servicemp3_methods[] =
 {
-	 {NULL,NULL,0,NULL}
+	{NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef moduledef = {
+		PyModuleDef_HEAD_INIT,
+		"servicemp3",							/* m_name */
+		"Module that implements gstreamer.",				/* m_doc */
+		-1,								/* m_size */
+		servicemp3_methods,						/* m_methods */
+		NULL,								/* m_reload */
+		NULL,								/* m_traverse */
+		NULL,								/* m_clear */
+		NULL,								/* m_free */
 };
 
 PyMODINIT_FUNC
 initservicemp3(void)
 {
-	Py_InitModule("servicemp3", servicemp3Methods);
+	return PyModule_Create(&moduledef);
 }
